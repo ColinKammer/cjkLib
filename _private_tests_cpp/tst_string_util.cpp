bca2682ea91073aa76f9a977f8bbf1fe5d0cec5f
ColinKammer <colin.justus.kammer qiem.thm.de>
2020-10-04 12:45:02 +0200

    Add Simple itoa (c++)

--- _private_tests_cpp/tst_string_util.cpp
+++ _private_tests_cpp/tst_string_util.cpp
@@ -14,5 +14,12 @@ int tst_stringUtil()
 	incrementIfFalse(errors, doCheck("normal_item0", (split("tst1 tst2", ' ').at(0) == "tst1"), "Unexpected Result"));
 	incrementIfFalse(errors, doCheck("normal_item1", (split("tst1 tst2", ' ').at(1) == "tst2"), "Unexpected Result"));
 
+
+	incrementIfFalse(errors, doCheck("positive", (cjk::itoa(123) == "123"), "Unexpected Result"));
+	incrementIfFalse(errors, doCheck("zero", (cjk::itoa(0) == "0"), "Unexpected Result"));
+	incrementIfFalse(errors, doCheck("negative", (cjk::itoa(-5) == "-5"), "Unexpected Result"));
+	incrementIfFalse(errors, doCheck("big", (cjk::itoa(1234567890) == "1234567890"), "Unexpected Result"));
+
+
 	return errors;
 }
--- string/string_util.h
+++ string/string_util.h
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <vector>
+#include <stack>
 
 namespace cjk {
 	static std::vector<std::string> split(const std::string& s, const char delim) {
@@ -22,6 +23,37 @@ namespace cjk {
 			}
 		}
 		result.push_back(currentString);
+
+		return result;
+	}
+
+	static std::string itoa(int32_t i) {
+		std::stack<char> digits;
+		std::string result;
+
+		if (i == 0)
+		{
+			return "0";
+		}
+
+		if (i < 0)
+		{
+			result.push_back('-');
+			i = -i;
+		}
+
+		while (i > 0)
+		{
+			auto currentDigit = i % 10;
+			digits.push(currentDigit);
+			i = i / 10;
+		}
+
+		while (digits.empty() == false)
+		{
+			result.push_back(digits.top() + '0');
+			digits.pop();
+		}
 
 		return result;
 	}
