2ebfcf8e446e0f0f2c96df2cbc3c0cb1d1aa34b0
ColinKammer <colin.justus.kammer qiem.thm.de>
2021-02-08 20:30:42 +0100

    Add RandomString to StringUtil

--- _private_tests_cpp/tst_string_util.cpp
+++ _private_tests_cpp/tst_string_util.cpp
@@ -3,23 +3,40 @@
 
 #include "test_helper.h"
 #include <iostream>
+#include <algorithm>
 
 using namespace cjk;
 
 int tst_stringUtil()
 {
-	int errors = 0;
+    int errors = 0;
 
-	incrementIfFalse(errors, doCheck("normal_size", (split("tst1 tst2", ' ').size() == 2), "Unexpected Result"));
-	incrementIfFalse(errors, doCheck("normal_item0", (split("tst1 tst2", ' ').at(0) == "tst1"), "Unexpected Result"));
-	incrementIfFalse(errors, doCheck("normal_item1", (split("tst1 tst2", ' ').at(1) == "tst2"), "Unexpected Result"));
-
-
-	incrementIfFalse(errors, doCheck("positive", (cjk::itoa(123) == "123"), "Unexpected Result"));
-	incrementIfFalse(errors, doCheck("zero", (cjk::itoa(0) == "0"), "Unexpected Result"));
-	incrementIfFalse(errors, doCheck("negative", (cjk::itoa(-5) == "-5"), "Unexpected Result"));
-	incrementIfFalse(errors, doCheck("big", (cjk::itoa(1234567890) == "1234567890"), "Unexpected Result"));
+    incrementIfFalse(errors, doCheck("normal_size", (split("tst1 tst2", ' ').size() == 2), "Unexpected Result"));
+    incrementIfFalse(errors, doCheck("normal_item0", (split("tst1 tst2", ' ').at(0) == "tst1"), "Unexpected Result"));
+    incrementIfFalse(errors, doCheck("normal_item1", (split("tst1 tst2", ' ').at(1) == "tst2"), "Unexpected Result"));
 
 
-	return errors;
+    incrementIfFalse(errors, doCheck("positive", (cjk::itoa(123) == "123"), "Unexpected Result"));
+    incrementIfFalse(errors, doCheck("zero", (cjk::itoa(0) == "0"), "Unexpected Result"));
+    incrementIfFalse(errors, doCheck("negative", (cjk::itoa(-5) == "-5"), "Unexpected Result"));
+    incrementIfFalse(errors, doCheck("big", (cjk::itoa(1234567890) == "1234567890"), "Unexpected Result"));
+
+    std::vector<std::string> randomStrings;
+    for (size_t i = 0; i < 300; i++)
+    {
+        randomStrings.push_back(randLowerCaseString(20));
+    }
+
+    //Check that every chacter is present in at least one of the 6000 generated: probility for false positive: < 10^-100
+    for (char c = 'a'; c <= 'z'; c++)
+    {
+        incrementIfFalse(errors, doCheck(
+            std::string("randomStringsContainsLetter_") + c,
+            std::any_of(randomStrings.begin(), randomStrings.end(), [c](std::string s) {return s.find(c) != std::string::npos; }),
+            "Unexpected Result"
+        ));
+
+    }
+
+    return errors;
 }
--- string/string_util.h
+++ string/string_util.h
@@ -5,56 +5,70 @@
 #include <stack>
 
 namespace cjk {
-	static std::vector<std::string> split(const std::string& s, const char delim) {
-		std::vector<std::string> result;
+    static std::vector<std::string> split(const std::string& s, const char delim) {
+        std::vector<std::string> result;
 
-		std::string currentString;
+        std::string currentString;
 
-		for (size_t i = 0; i < s.length(); i++)
-		{
-			if (s[i] == delim)
-			{
-				result.push_back(currentString);
-				currentString = std::string();
-			}
-			else
-			{
-				currentString += s[i];
-			}
-		}
-		result.push_back(currentString);
+        for (size_t i = 0; i < s.length(); i++)
+        {
+            if (s[i] == delim)
+            {
+                result.push_back(currentString);
+                currentString = std::string();
+            }
+            else
+            {
+                currentString += s[i];
+            }
+        }
+        result.push_back(currentString);
 
-		return result;
-	}
+        return result;
+    }
 
-	static std::string itoa(int32_t i) {
-		std::stack<char> digits;
-		std::string result;
+    static std::string itoa(int32_t i) {
+        std::stack<char> digits;
+        std::string result;
 
-		if (i == 0)
-		{
-			return "0";
-		}
+        if (i == 0)
+        {
+            return "0";
+        }
 
-		if (i < 0)
-		{
-			result.push_back('-');
-			i = -i;
-		}
+        if (i < 0)
+        {
+            result.push_back('-');
+            i = -i;
+        }
 
-		while (i > 0)
-		{
-			auto currentDigit = i % 10;
-			digits.push(currentDigit);
-			i = i / 10;
-		}
+        while (i > 0)
+        {
+            auto currentDigit = i % 10;
+            digits.push(currentDigit);
+            i = i / 10;
+        }
 
-		while (digits.empty() == false)
-		{
-			result.push_back(digits.top() + '0');
-			digits.pop();
-		}
+        while (digits.empty() == false)
+        {
+            result.push_back(digits.top() + '0');
+            digits.pop();
+        }
 
-		return result;
-	}
+        return result;
+    }
+
+    static std::string randLowerCaseString(size_t length)
+    {
+        //may be low entropy: DO NOT USE FOR ANY CRYPTOGRAPHIC PURPOSES
+        std::string outStr(length, 'X');
+
+        for (size_t i = 0; i < length; i++)
+        {
+            auto r = rand() % 26;
+            outStr[i] = 'a' + r;
+        }
+
+        return outStr;
+    }
 }
