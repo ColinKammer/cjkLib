a466f4cb7354a2a070a263ed3bee0149c73d8487
ColinKammer <colin.justus.kammer qiem.thm.de>
2021-06-29 22:41:12 +0200

    Test createPointerVector

--- _private_tests_cpp/tst_vector_util.cpp
+++ _private_tests_cpp/tst_vector_util.cpp
@@ -15,7 +15,6 @@ int tst_vectorUtil()
 	std::vector<int> v2 = { 5, 6, 7, 8 };
 	push_back_vectorOnVector(v1, v2);
 	
-
 	incrementIfFalse(errors, doCheck("length", (v1.size() == 8), "Unexpected Result"));
 	incrementIfFalse(errors, doCheck("value", (v1.at(0) == 1), "Unexpected Result"));
 	incrementIfFalse(errors, doCheck("value", (v1.at(1) == 2), "Unexpected Result"));
@@ -25,6 +24,16 @@ int tst_vectorUtil()
 	incrementIfFalse(errors, doCheck("value", (v1.at(5) == 6), "Unexpected Result"));
 	incrementIfFalse(errors, doCheck("value", (v1.at(6) == 7), "Unexpected Result"));
 	incrementIfFalse(errors, doCheck("value", (v1.at(7) == 8), "Unexpected Result"));
+
+	auto v1ptr = createPointerVector(v1);
+	auto v1ptr0 = v1ptr[0];
+	static_assert(std::is_same_v<decltype(v1ptr0), int*>,"Incorrect type for pointerVector-Elemts");
+
+	incrementIfFalse(errors, doCheck("ptr length", (v1ptr.size() == v1.size()), "Unexpected Result"));
+	incrementIfFalse(errors, doCheck("ptr 0 value", (v1ptr[0] == &v1[0]), "Unexpected Result"));
+	incrementIfFalse(errors, doCheck("ptr 0 value", (v1ptr[1] == &v1[1]), "Unexpected Result"));
+	incrementIfFalse(errors, doCheck("ptr 0 value", (v1ptr[2] == &v1[2]), "Unexpected Result"));
+	incrementIfFalse(errors, doCheck("ptr 0 value", (v1ptr[3] == &v1[3]), "Unexpected Result"));
 
 	return errors;
 }
